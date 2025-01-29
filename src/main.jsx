005f3aefa0b366bd5f3334c25149e7c533e001c7
Nazar Buk <nazarbuk27@gmail.com>
2025-01-29 14:57:35 +0200

    👔 Clear default App and add structures of files
    📦️ Add sass package.
    🏗️ Create scss files and folders structure.
    🏗️ Create router structure.
    ✏️ Connect fonts.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: "page1",
+      },
+      {
+        path: "/page2",
+        element: "page2",
+      },
+      {
+        path: "/page3",
+        element: "page3",
+      },
+      {
+        path: "/page4",
+        element: "page4",
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>
+  <RouterProvider router={router} fallbackElement={<>Loading...</>} />
+);
