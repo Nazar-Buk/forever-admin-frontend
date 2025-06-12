8637fe8681833d5b4a6f592cdafea763ba4196bc
Nazar Buk <nazarbuk27@gmail.com>
2025-06-12 17:06:03 +0300

    Change vite configs

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 5174 }, // щоб на цьому потрі запускався,
-  base: "/forever-admin-frontend/",
+  // base: "/forever-admin-frontend/", // розкоментуй base щоб дивитися проект локально
+  base: "/", // для того щоб можна було розгорнути на хостингу
 });
